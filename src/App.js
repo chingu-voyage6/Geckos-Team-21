903ae5a96661e22b70a189caada3744a36947462
Zoey <zoey.rezaee@gmail.com>
2018-06-23 02:58:56 +0430

    starting header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
+import Header from "./Header.js";
 import "./App.css";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Giveaway App!!!! </h1>
+        <Header />
+        
       </div>
     );
   }
