fd665b862517d99dd84b910cf1c17262ee5f5204
Zoey <zoey.rezaee@gmail.com>
2018-06-26 15:34:58 +0430

    changed server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/src', express.static(__dirname + '/src'));
 
 app.get('/', function(req,res) {
-	res.sendFile(__dirname + "/views/home-page.html");
+	res.sendFile(__dirname + "/app/views/index.html");
 })
 
 var port = "8080"
