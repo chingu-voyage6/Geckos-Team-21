5bbd3072306f7b5b1fee43bd4f4687e010d84f7b
Marco <vecchio.m@gmail.com>
2018-06-26 22:56:16 +0200

    introducing db

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var mysql = require('mysql');
+
 var app = express();
 
-
 app.use('/public', express.static(__dirname + '/public'));
+
+var connection = mysql.createConnection({
+	host: ' xyz',  //still to identify how to spot host name on heroku
+	user: 'xyz', //still to identify how to spot user name on heroku
+	database: 'clean_app' //name of the database
+});
+
 
 app.get('/', function(req,res) {
 	res.sendFile(__dirname + "/views/home-page.html");
