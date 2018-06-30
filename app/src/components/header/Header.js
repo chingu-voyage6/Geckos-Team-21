ea2b546bc785c924dbe9d9ab4f81d478c221e664
Zoey <zoey.rezaee@gmail.com>
2018-07-01 01:42:06 +0430

    style navbar

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -1,29 +1,45 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./Header.css";
+
+// navbar component
 class Navbar extends Component{
     render(){
+        const pages = ['login','post an ad', 'categories', 'about', 'home'];
+        const navLinks = pages.map((page) => {
+            return(
+                <a href="#">{page}</a>
+            )
+        });
         return(
             <div class="Navbar">
-            <ul>
-                <li><a href="#">home</a></li>
-                <li><a href="#">categories</a></li>
-                <li><a href="#">about</a></li>
-                <li><a href="#">login</a></li>
-            </ul>
+            {navLinks}
             </div>
         )
     }
 }
+
+// Banner component
+class Banner extends Component{
+    render(){
+        return(
+            <div>
+                <h1> clean app! </h1>
+                <h2>why wasting when you can share?!</h2>
+                <div className="PinkCircle"></div>
+                <div className="BlueCircle"></div>
+                <div className="YellowCircle"></div>
+            </div>   
+        )
+    }
+}
+
 class Header extends Component{
     render(){
         return(
           <div className="Header">
             <Navbar />
-            <h1> clean app! </h1>
-            <div className="PinkCircle"></div>
-            <div className="BlueCircle"></div>
-            <div className="YellowCircle"></div>
+            <Banner />
           </div>
         );
       }
