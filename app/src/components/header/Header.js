feb6e7a675559d28f796dd1671703b70d07c77c1
Zoey <zoey.rezaee@gmail.com>
2018-07-01 16:53:20 +0430

    change style

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -1,38 +1,9 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./Header.css";
+import Navbar from "./Navbar";
+import Banner from "./Banner";
 
-// navbar component
-class Navbar extends Component{
-    render(){
-        const pages = ['login','post an ad', 'categories', 'about', 'home'];
-        const navLinks = pages.map((page) => {
-            return(
-                <a href="#">{page}</a>
-            )
-        });
-        return(
-            <div class="Navbar">
-            {navLinks}
-            </div>
-        )
-    }
-}
-
-// Banner component
-class Banner extends Component{
-    render(){
-        return(
-            <div>
-                <h1> clean app! </h1>
-                <h2>why wasting when you can share?!</h2>
-                <div className="PinkCircle"></div>
-                <div className="BlueCircle"></div>
-                <div className="YellowCircle"></div>
-            </div>   
-        )
-    }
-}
 
 class Header extends Component{
     render(){
