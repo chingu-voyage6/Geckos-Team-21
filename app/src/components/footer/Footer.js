9e57202b410fb02f9c349c9a5a922832797fdae9
Zoey <zoey.rezaee@gmail.com>
2018-07-01 16:52:13 +0430

    add footer

---
+++ app/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {hot} from "react-hot-loader";
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <p>Geckos Team21 2018 + All rights reserved</p>
+        </div>
+    );
+}
+export default hot(module)(Footer);
