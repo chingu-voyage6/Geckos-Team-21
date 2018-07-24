4bffbe6903fcca50a4e46fabdd7996d9539bb033
Zoey <zoey.rezaee@gmail.com>
2018-07-25 03:24:45 +0430

    editted and styleed forms

--- app/src/components/addItem.js
+++ app/src/components/addItem.js
@@ -14,41 +14,44 @@ class addItem extends Component{
   		return (<option key={categories.indexOf(cat)} value={cat}>{cat}</option>)
   	})
     return(
-      <div className="addItem">        
-            <form encType="multipart/form-data"  method="POST" action="/api/addItem">
+      <div className="form">  
+				<h1>add a new item</h1>      
+          <form encType="multipart/form-data"  method="POST" action="/api/addItem">
 			
-			<label htmlFor="title">Title:</label>
-			<input type="text" name="title" id="title" /><br />
+						<label htmlFor="title">Title:</label>
+						<input type="text" name="title" id="title" />
 
-			<label htmlFor="category">Choose category:</label>
-			<select  name="category" id="category">
-				{ selectFields }
-			</select>
-			<br />
+						<label htmlFor="category">Choose category:</label>
+						<select  name="category" id="category">
+							{ selectFields }
+						</select>
 
-			<label htmlFor="country">Country:</label>
-			<input type="text" name="country" id="country" /><br />
+						<label htmlFor="country">Country:</label>
+						<input type="text" name="country" id="country" />
 
-			<label htmlFor="city">City:</label>
-			<input type="text" name="city" id="city" /><br />
+						<label htmlFor="city">City:</label>
+						<input type="text" name="city" id="city" />
 
-			<label htmlFor="description">Description:</label>
-			<textarea name="description" id="description" rows="5" defaultValue="Item description"></textarea>
-			<br />
+						<label htmlFor="description">Description:</label>
+						<textarea name="description" id="description" rows="5" defaultValue="Item description"></textarea>
+						<br />
 
-			<label htmlFor="expirationDate">Expiration date:</label>
-			<input type="date" name="expirationDate" id="expirationDate" /><br />
+						<label htmlFor="expirationDate">Expiration date:</label>
+						<input type="date" name="expirationDate" id="expirationDate" />
 
-			<label htmlFor="tel">Phone number:</label>
-			<input type="tel" name="tel" id="tel" /><br />
+						<label htmlFor="price">Price:</label>
+						<input type="text" name="price" id="price" />
+
+						<label htmlFor="tel">Phone number:</label>
+						<input type="tel" name="tel" id="tel" />
 
 
-			<label htmlFor="photos">Photos:</label>
-			<input type="file" multiple name="photos" accept="image/*" id="photos" /><br />
+						<label htmlFor="photos">Photos:</label>
+						<input type="file" multiple name="photos" accept="image/*" id="photos" />
 
-			<input type="submit" value="Add Item" id="add" />
+						<input type="submit" value="Add Item" id="add" />
 
-		</form>       
+				</form>       
       </div>
     );
   }
