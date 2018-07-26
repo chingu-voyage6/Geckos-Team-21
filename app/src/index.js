d8e70cf692c57b581f45aca3e028f0e34e42c105
Zoey <zoey.rezaee@gmail.com>
2018-07-27 02:37:04 +0430

    added redux and redux-react

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
 import AppRouter from "./routers/AppRouter";
+import configureStore from './store/configureStore';
+import {addItem} from './actions/items';
+import {setTextFilter, setCategoryFilter, setSortByPrice} from './actions/filters';
+import getVisibleItems from './selectors/visibleItems';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+ ////////////////////////////////////////////////
+const store = configureStore();
+
+store.dispatch(addItem({title: 'rug', price: 100, category: 'furniture'}));
+store.dispatch(addItem({title: 'lamp', price: 300, category: 'furniture'}));
+store.dispatch(addItem({title: 'spagetti', price: 200, category: 'food'}));
 
 
-ReactDOM.render(
-	<AppRouter />,
-	document.getElementById("root")
+const state = store.getState();
+
+////////////////////////////////////////////////
+
+const jsx = (
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
 );
+ReactDOM.render(jsx,document.getElementById("root"));
