f25fef8a28266f35dcc31aabaec84ec437f3724a
Zoey <zoey.rezaee@gmail.com>
2018-07-27 01:17:59 +0430

    controlled Components

--- app/src/components/addItem.js
+++ app/src/components/addItem.js
@@ -4,10 +4,26 @@ import {hot} from "react-hot-loader";
 
 class addItem extends Component{
   constructor(props) {
-  	super(props); 
+		super(props); 
+		this.state={
+			title: '',
+			category: 'Games',
+			country: '',
+			city: '',
+			description: '',
+			expirationdate: '',
+			price: '',
+			tel :''
+		}
+		this.handleInputChange = this.handleInputChange.bind(this);
   }
   
-
+	handleInputChange(e){
+		const target = e.target
+		this.setState(() => ({
+			[target.name] : target.value
+		}));
+  }
   render(){
   	let categories = ["Games", "Movies", "Books", "Magazines", "Clothes", "Food", "Furniture", "Bikes", "Weird things"];
   	const selectFields = categories.map(function(cat) {
@@ -16,40 +32,40 @@ class addItem extends Component{
     return(
       <div className="form">  
 				<h1>add a new item</h1>      
-          <form encType="multipart/form-data"  method="POST" action="/api/addItem">
-			
-						<label htmlFor="title">Title:</label>
-						<input type="text" name="title" id="title" />
+				<form encType="multipart/form-data"  method="POST" action="/api/addItem">
+				
+					<label htmlFor="title">Title:</label>
+					<input type="text" name="title" id="title" value={this.state.title} onChange={this.handleInputChange} />
 
-						<label htmlFor="category">Choose category:</label>
-						<select  name="category" id="category">
-							{ selectFields }
-						</select>
+					<label htmlFor="category">Choose category:</label>
+					<select  name="category" id="category" value={this.state.category}  onChange={this.handleInputChange}>
+						{ selectFields }
+					</select>
 
-						<label htmlFor="country">Country:</label>
-						<input type="text" name="country" id="country" />
+					<label htmlFor="country">Country:</label>
+					<input type="text" name="country" id="country" value={this.state.country} onChange={this.handleInputChange}/>
 
-						<label htmlFor="city">City:</label>
-						<input type="text" name="city" id="city" />
+					<label htmlFor="city">City:</label>
+					<input type="text" name="city" id="city" value={this.state.city}  onChange={this.handleInputChange} />
 
-						<label htmlFor="description">Description:</label>
-						<textarea name="description" id="description" rows="5" defaultValue="Item description"></textarea>
-						<br />
+					<label htmlFor="description">Description:</label>
+					<textarea name="description" id="description" rows="5" value={this.state.description} 
+					onChange={this.handleInputChange}></textarea>
 
-						<label htmlFor="expirationDate">Expiration date:</label>
-						<input type="date" name="expirationDate" id="expirationDate" />
+					<label htmlFor="expirationDate">Expiration date:</label>
+					<input type="date" name="expirationDate" id="expirationDate" value={this.state.expirationdate}  onChange={this.handleInputChange} />
 
-						<label htmlFor="price">Price:</label>
-						<input type="text" name="price" id="price" />
+					<label htmlFor="price">Price:</label>
+					<input type="text" name="price" id="price" value={this.state.price}  onChange={this.handleInputChange} />
 
-						<label htmlFor="tel">Phone number:</label>
-						<input type="tel" name="tel" id="tel" />
+					<label htmlFor="tel">Phone number:</label>
+					<input type="tel" name="tel" id="tel" value={this.state.tel} onChange={this.handleInputChange}/>
 
 
-						<label htmlFor="photos">Photos:</label>
-						<input type="file" multiple name="photos" accept="image/*" id="photos" />
+					<label htmlFor="photos">Photos:</label>
+					<input type="file" multiple name="photos" accept="image/*" id="photos" />
 
-						<input type="submit" value="Add Item" id="add" />
+					<input type="submit" value="Add Item" id="add" />
 
 				</form>       
       </div>
