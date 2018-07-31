4e1458582ba16fc780866483ec4ba1078be35908
Zoey <zoey.rezaee@gmail.com>
2018-07-31 18:46:08 +0430

    change it to stateless component

--- app/src/components/main/items-block/Items-block.js
+++ app/src/components/main/items-block/Items-block.js
@@ -3,23 +3,11 @@ import {hot} from "react-hot-loader";
 import ItemsFilters from './ItemsFilters';
 import ItemsList from './ItemsList';
 
-class ItemsBlock extends Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <div>
-                <ItemsFilters />
-                <ItemsList />
-            </div>
-        )
-    }
-}
-ItemsBlock.defaultProps = {
-    itemTitle: 'Title',
-    src: '#',
-    itemDetails: 'detail'
-};
+const ItemsBlock = () => (
+    <div>
+        <ItemsFilters />
+        <ItemsList />
+    </div>
+)
 
 export default hot(module)(ItemsBlock);
