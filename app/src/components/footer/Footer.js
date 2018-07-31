27963c3d97ed75b173813f9c6184498dd78104d7
Zoey <zoey.rezaee@gmail.com>
2018-07-31 18:51:48 +0430

    change to stateless function

--- app/src/components/footer/Footer.js
+++ app/src/components/footer/Footer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {hot} from "react-hot-loader";
 
-const Footer = () => {
-    return(
-        <div className="footer">
-            <p>Geckos Team21 2018 + All rights reserved</p>
-        </div>
-    );
-}
+const Footer = () => (
+    <div className="footer">
+        <p>Geckos Team21 2018 + All rights reserved</p>
+    </div>
+);
 export default hot(module)(Footer);
