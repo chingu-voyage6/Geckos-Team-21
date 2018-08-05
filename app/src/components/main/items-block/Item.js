d333825c83b5fd5fa82902f2d92cab38e87e1275
Zoey <zoey.rezaee@gmail.com>
2018-08-05 16:02:11 +0430

    add country and city detail to each item

--- app/src/components/main/items-block/Item.js
+++ app/src/components/main/items-block/Item.js
@@ -3,9 +3,10 @@ import {hot} from "react-hot-loader";
 import { Link } from 'react-router-dom';
 //import img from './file.jpg';
 
-const Item = ({id, title, price, photo}) => (
+const Item = ({id, title, country, city, price, photo}) => (
     <div className="item-card">
-        <h1>{title}</h1>
+        <h2>{title}</h2>
+        <p>{country},{city}</p>
         <img src={photo} alt="" /> 
         <p>Price: {price} <Link to={`/item/${id}`} key={id} className="item-link">details</Link></p>
     </div>
