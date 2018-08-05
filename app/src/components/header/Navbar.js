935a5d07f1c71734a93b47ff359c0b07321be904
Zoey <zoey.rezaee@gmail.com>
2018-08-05 16:01:39 +0430

    make navbar responsive

--- app/src/components/header/Navbar.js
+++ app/src/components/header/Navbar.js
@@ -1,13 +1,21 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 // navbar component
 class Navbar extends Component{
     constructor(props) {
-    super(props);
+        super(props);
+        this.handleClick= this.handleClick.bind(this);
   }
-
+    handleClick(){
+        let dropDown = document.querySelector('.dropdown');
+        dropDown.classList.contains('dropdown-active')?
+        dropDown.classList.remove('dropdown-active'):
+        dropDown.classList.add('dropdown-active');
+    }
     render(){
 
         let pages = [['login', '/login'], ['Sign up', '/register'], ['post an ad', '/addNewItem'], ['categories', '#'], ['about', '#'], ['home', '#']];
@@ -20,13 +28,29 @@ class Navbar extends Component{
         }
         const navLinks = pages.map((page) => {
             return(
-                <Link to={page[1]} key={pages.indexOf(page)}>{page[0]}</Link>
+                <Link className="navbar-link" to={page[1]} key={pages.indexOf(page)}>{page[0]}</Link>
             )
         });
 
         return(
-            <div className="Navbar">
-            { navLinks }            
+            <div>
+                <div className="navbar laptop-navbar">
+                { navLinks }            
+                </div>
+                <div className ="navbar mobile-navbar">
+                    {navLinks[0]}
+                    {navLinks[1]}
+                    <button className="bars" onClick={this.handleClick}>
+                        <FontAwesomeIcon icon={faBars} size="2x" />
+                    </button>
+                    <div className="dropdown">
+                    {navLinks[2]}
+                    {navLinks[3]}
+                    {navLinks[4]}
+                    {navLinks[5]}
+                </div>
+                </div>
+                
             </div>
         )
     }
